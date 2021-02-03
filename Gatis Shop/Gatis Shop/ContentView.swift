d93f24c40e9c8faec7035d72cbb5d21e335045e4
Roberto Seittiffe <robertoseittiffe@icloud.com>
2021-02-03 01:52:33 -0300

    iOS app incubation process start

---
+++ Gatis Shop/Gatis Shop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gatis Shop
+//
+//  Created by Roberto Seittiffe on 03-02-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gatis Shop/Gatis Shop/Gatis_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gatis_ShopApp.swift
+//  Gatis Shop
+//
+//  Created by Roberto Seittiffe on 03-02-21.
+//
+
+import SwiftUI
+
+@main
+struct Gatis_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
